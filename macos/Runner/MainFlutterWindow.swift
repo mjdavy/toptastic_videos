a56193a2aa2b0a3ca5467020830322e8a348fab4
Martin Davy <martin.davy@gmail.com>
2025-12-05 11:48:22 -0500

    added mac build

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -8,6 +8,15 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    // SCREENSHOT MODE: Uncomment ONE of these lines to set window size for App Store screenshots
+    // Then take screenshot with Cmd+Shift+4, press Space, click window
+    // Remember to comment out again before releasing!
+    
+    //setContentSize(NSSize(width: 1280, height: 800))   // 13" MacBook
+    // setContentSize(NSSize(width: 1440, height: 900))   // 13" MacBook Pro
+    //setContentSize(NSSize(width: 2560, height: 1600))  // 13" Retina
+    //setContentSize(NSSize(width: 2880, height: 1800))  // 15" Retina
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
